76a088dcc0a25b6982799fcb2b712abecb64970b
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-03-01 08:12:48 +0700

    Create SplitViewController.swift

---
+++ tdMouse/tdMouse/UIs/SplitViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SplitViewController.swift
+//  tdMouse
+//
+//  Created by Nguyen, Thinh on 28/2/25.
+//
+
+import Cocoa
+
+class SplitViewController: NSSplitViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
