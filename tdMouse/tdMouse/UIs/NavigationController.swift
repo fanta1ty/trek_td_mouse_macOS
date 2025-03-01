bbbe3a7ea7a19aef67e5eff3d466dacd731ceec6
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-03-01 08:12:50 +0700

    Create NavigationController.swift

---
+++ tdMouse/tdMouse/UIs/NavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationController.swift
+//  tdMouse
+//
+//  Created by Nguyen, Thinh on 28/2/25.
+//
+
+import Cocoa
+
+class NavigationController: NSViewController {
+    static let navigationDidFinished = Notification.Name("NavigationControllerNavigationDidFinished")
+
+    override func swipe(with event: NSEvent) {
+        
+    }
+}
