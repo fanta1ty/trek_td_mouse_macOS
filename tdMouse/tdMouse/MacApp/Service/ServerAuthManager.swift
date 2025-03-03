1e8cdbe3dabeda001845d94bd97a6d5748515210
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2025-03-04 00:11:58 +0700

    Create ServerAuthManager.swift

---
+++ tdMouse/tdMouse/MacApp/Service/ServerAuthManager.swift
@@ -0,0 +1,109 @@
+//
+//  ServerAuthManager.swift
+//  tdMouse
+//
+//  Created by Thinh Nguyen on 3/3/25.
+//
+
+import Cocoa
+import SMBClient
+
+class ServerAuthManager: AuthManager {
+    private let id: ID
+    
+    private var authWindowController: ConnectServerWindowController
+    private var authViewController: ConnectServerViewController
+    
+    private var session: Session?
+    
+    init(id: ID) {
+        self.id = id
+        
+        authWindowController = ConnectServerWindowController.instantiate()
+        
+        authViewController = authWindowController.contentViewController as! ConnectServerViewController
+        authViewController.connectButton.target = self
+        authViewController.cancelButton.target = self
+        
+        authViewController.connectButton.action = #selector(connectAction(_:))
+        authViewController.cancelButton.action = #selector(cancelAction(_:))
+    }
+    
+    func authenticate() -> Session? {
+        if let session = SessionManager.shared.session(for: id) {
+            return session
+        }
+        
+        if let server = ServerManager.shared.server(for: id) {
+            authViewController.displayNameField.stringValue = server.displayName
+            authViewController.serverField.stringValue = server.server
+            
+            if let port = server.port {
+                authViewController.portField.stringValue = "\(port)"
+            }
+            
+            let store = CredentialStore.shared
+            if let credential = store.load(server: server.server, securityDomain: id.rawValue) {
+                authViewController.usernameField.stringValue = credential.username
+                authViewController.passwordField.stringValue = credential.password
+            }
+        }
+        
+        authViewController.connectButton.isEnabled = !authViewController.usernameField.stringValue.isEmpty
+        
+        NSApp.runModal(for: authWindowController.window!)
+        return session
+    }
+}
+
+// MARK:  - Action Functions
+extension ServerAuthManager {
+    @objc private func connectAction(_ sender: Any) {
+        authViewController.connectButton.isEnabled = false
+        
+        let displayName = authViewController.displayNameField.stringValue
+        let server = authViewController.serverField.stringValue
+        let port = authViewController.portField.integerValue
+        let username = authViewController.usernameField.stringValue
+        let password = authViewController.passwordField.stringValue
+        let rememberPassword = authViewController.rememberPasswordCheckbox.state == .on
+        
+        Task { @MainActor in
+            do {
+                let sessionManager = SessionManager.shared
+                let session = try await sessionManager.login(
+                    id: id,
+                    displayName: displayName,
+                    server: server,
+                    port: port == 0 ? nil : port,
+                    username: username,
+                    password: password,
+                    savePassword: rememberPassword
+                )
+                self.session = session
+                
+                NSApp.stopModal(withCode: .OK)
+                authWindowController.window?.orderOut(nil)
+            } catch {
+                await MainActor.run {
+                    if let error = error as? ErrorResponse, NTStatus(error.header.status) == .logonFailure {
+                        authWindowController.window?.performSelector(
+                            onMainThread: NSSelectorFromString("_shake"),
+                            with: nil,
+                            waitUntilDone: true
+                        )
+                    } else {
+                        NSAlert(error: error).runModal()
+                    }
+                    
+                    authViewController.connectButton.isEnabled = true
+                }
+            }
+        }
+    }
+    
+    @objc func cancelAction(_ sender: Any) {
+        NSApp.stopModal(withCode: .cancel)
+        authWindowController.window?.orderOut(nil)
+    }
+}
