fef14446103372779d26a95714eb1ba19566dca7
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2025-03-04 00:11:39 +0700

    Create Credential.swift

---
+++ tdMouse/tdMouse/MacApp/Model/Credential.swift
@@ -0,0 +1,12 @@
+//
+//  Credential.swift
+//  tdMouse
+//
+//  Created by Thinh Nguyen on 3/3/25.
+//
+
+struct Credential {
+    let server: String
+    let username: String
+    let password: String
+}
