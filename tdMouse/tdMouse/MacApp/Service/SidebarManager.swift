37fda358a05a43dc320c6ca4b71413619b8bbd02
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2025-03-04 11:14:48 +0700

    Create SidebarManager.swift

---
+++ tdMouse/tdMouse/MacApp/Service/SidebarManager.swift
@@ -0,0 +1,16 @@
+//
+//  SidebarManager.swift
+//  tdMouse
+//
+//  Created by Thinh Nguyen on 4/3/25.
+//
+
+import Cocoa
+import SMBClient
+
+class SidebarManager {
+    static let shared = SidebarManager()
+    static let sidebarDidUpdate = Notification.Name("SidebarManagerSidebarDidUpdate")
+    
+    
+}
