9a8f9cf4f1e31c623d3b8216f158509d6ba807de
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2025-03-05 00:53:17 +0700

    Update SidebarViewController.swift

--- tdMouse/tdMouse/MacApp/UI/SidebarViewController.swift
+++ tdMouse/tdMouse/MacApp/UI/SidebarViewController.swift
@@ -15,15 +15,75 @@ class SidebarViewController: NSViewController {
         
         sourceList.dataSource = self
         sourceList.delegate = self
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(sidebarDidUpdate(_:)),
+            name: SidebarManager.sidebarDidUpdate,
+            object: nil
+        )
     }
 }
 
 // MARK: - NSOutlineViewDataSource
 extension SidebarViewController: NSOutlineViewDataSource {
+    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+        SidebarManager.shared.numberOfChildrenOfItem(item)
+    }
     
+    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+        SidebarManager.shared.child(index, ofItem: item)
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+        SidebarManager.shared.isItemExpandable(item)
+    }
 }
 
 // MARK: - NSOutlineViewDelegate
 extension SidebarViewController: NSOutlineViewDelegate {
-    
+    func outlineView(
+        _ outlineView: NSOutlineView,
+        viewFor tableColumn: NSTableColumn?,
+        item: Any
+    ) -> NSView? {
+        guard let node = item as? SidebarNode else { return nil }
+        
+        switch node.content {
+        case let headerNode as HeaderNode:
+            let cellIdentifier = NSUserInterfaceItemIdentifier("HeaderCell")
+            guard let cell = outlineView.makeView(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView else { return nil }
+            cell.textField?.stringValue = headerNode.name
+            return cell
+            
+        case let serverNode as ServerNode:
+            let cellIdentifier = NSUserInterfaceItemIdentifier("DataCell")
+            guard let cell = outlineView.makeView(withIdentifier: cellIdentifier, owner: nil) as? SidebarCellView else { return nil }
+            
+            cell.imageView?.image = NSImage(systemSymbolName: "server.rack", accessibilityDescription: nil)
+            cell.textField?.stringValue = serverNode.name
+            
+            cell.ejectButton.isHidden = !SessionManager.shared.sessionExists(for: serverNode.id)
+            cell.ejectAction = {
+                cell.ejectButton.isEnabled = false
+                
+                Task { @MainActor in
+                    await SidebarManager.shared.logoff(node)
+                    cell.ejectButton.isEnabled = true
+                }
+            }
+            
+            
+            return cell
+            
+        default: return nil
+        }
+    }
+}
+
+// MARK: - Private Functions
+extension SidebarViewController {
+    @objc private func sidebarDidUpdate(_ notification: Notification) {
+      sourceList.reloadData()
+    }
 }
