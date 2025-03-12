d2f6b402aa1178a5495e01a2459c2b840268ac96
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2025-03-12 14:49:58 +0700

    Update SplitViewController.swift

--- tdMouse/tdMouse/UI/ViewControllers/SplitViewController.swift
+++ tdMouse/tdMouse/UI/ViewControllers/SplitViewController.swift
@@ -3,5 +3,14 @@ import Cocoa
 class SplitViewController: NSSplitViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let sidebarVC = SidebarViewController()
+        let mainVC = NavigationController()
+        
+        let sidebarItem = NSSplitViewItem(sidebarWithViewController: sidebarVC)
+        let mainItem = NSSplitViewItem(viewController: mainVC)
+        
+        addSplitViewItem(sidebarItem)
+        addSplitViewItem(mainItem)
     }
 }
