47ad2eb3e640032e614ec1d25fd8234d3ffd7d42
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-03-22 20:21:29 +0700

    Update TransferOperation.swift

--- tdMouse/tdMouse/Models/Enums/TransferOperation.swift
+++ tdMouse/tdMouse/Models/Enums/TransferOperation.swift
@@ -10,4 +10,19 @@ enum TransferOperation: Equatable {
     case downloading(String)
     case uploading(String)
     case listing(String)
+    
+    static func == (lhs: TransferOperation, rhs: TransferOperation) -> Bool {
+        switch (lhs, rhs) {
+        case (.none, .none):
+            return true
+        case (.listing(let lhsPath), .listing(let rhsPath)):
+            return lhsPath == rhsPath
+        case (.downloading(let lhsFile), .downloading(let rhsFile)):
+            return lhsFile == rhsFile
+        case (.uploading(let lhsFile), .uploading(let rhsFile)):
+            return lhsFile == rhsFile
+        default:
+            return false
+        }
+    }
 }
