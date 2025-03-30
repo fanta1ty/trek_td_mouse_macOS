8c123ff091330211ca652e3d16afd8c14744b214
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-03-30 13:56:11 +0700

    Updated

---
+++ tdMouse/tdMouse_ios/tdMouse_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tdMouse_iosApp.swift
+//  tdMouse_ios
+//
+//  Created by mobile on 30/3/25.
+//
+
+import SwiftUI
+
+@main
+struct tdMouse_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
