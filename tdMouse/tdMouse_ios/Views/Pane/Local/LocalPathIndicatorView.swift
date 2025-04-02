8850ea22109531bcc0456d690eaa5940f8092045
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2025-04-02 23:50:56 +0700

    Create LocalPathIndicatorView.swift

---
+++ tdMouse/tdMouse_ios/Views/Pane/Local/LocalPathIndicatorView.swift
@@ -0,0 +1,34 @@
+//
+//  LocalPathIndicatorView.swift
+//  tdMouse
+//
+//  Created by Thinh Nguyen on 2/4/25.
+//
+
+import SwiftUI
+
+struct LocalPathIndicatorView: View {
+    @EnvironmentObject private var viewModel: LocalViewModel
+    
+    var body: some View {
+        HStack {
+            Text(viewModel.currentDirectory?.lastPathComponent ?? "")
+                .font(.caption)
+                .lineLimit(1)
+                .truncationMode(.middle)
+                .padding(.vertical, 4)
+                .padding(.horizontal, 8)
+                .background(Color.green.opacity(0.1))
+                .cornerRadius(4)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LocalPathIndicatorView_Previews: PreviewProvider {
+    static var previews: some View  {
+        LocalPathIndicatorView()
+            .environmentObject(LocalViewModel())
+    }
+}
