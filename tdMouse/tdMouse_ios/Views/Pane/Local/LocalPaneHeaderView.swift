d336043f61c4befa729fdfe2e6e1910cbc356240
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2025-04-02 23:50:54 +0700

    Create LocalPaneHeaderView.swift

---
+++ tdMouse/tdMouse_ios/Views/Pane/Local/LocalPaneHeaderView.swift
@@ -0,0 +1,41 @@
+//
+//  LocalPaneHeaderView.swift
+//  tdMouse
+//
+//  Created by Thinh Nguyen on 2/4/25.
+//
+
+import SwiftUI
+
+struct LocalPaneHeaderView: View {
+    @EnvironmentObject private var viewModel: LocalViewModel
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            HStack(spacing: 12) {
+                Button {
+                    viewModel.navigateUp()
+                } label: {
+                    Image(systemName: "arrow.up")
+                        .foregroundColor(viewModel.canNavigateUp ? .blue : .gray)
+                }
+                .disabled(!viewModel.canNavigateUp)
+                
+                Button {
+                    viewModel.refreshLocalFiles()
+                } label: {
+                    Image(systemName: "arrow.clockwise")
+                }
+            }
+        }
+    }
+}
+
+struct LocalPaneHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocalPaneHeaderView()
+            .environmentObject(LocalViewModel())
+    }
+}
