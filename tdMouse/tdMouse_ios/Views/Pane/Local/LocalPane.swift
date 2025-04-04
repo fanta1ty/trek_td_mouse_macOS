1fa309fb5beb24706395ab8f5cf3f00535caee55
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2025-04-04 23:03:51 +0700

    Update LocalPane.swift

--- tdMouse/tdMouse_ios/Views/Pane/Local/LocalPane.swift
+++ tdMouse/tdMouse_ios/Views/Pane/Local/LocalPane.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct LocalPane: View {
     @EnvironmentObject private var viewModel: LocalViewModel
+    @EnvironmentObject private var smbViewModel: FileTransferViewModel
+    @EnvironmentObject private var transferManager: TransferManager
     
     @Binding var currentPreviewFile: PreviewFileInfo?
     @Binding var activePaneIndex: Int
@@ -18,11 +20,11 @@ struct LocalPane: View {
         VStack(spacing: 0) {
             // Header
             LocalPaneHeaderView()
-            .padding(.vertical, 4)
+                .padding(.vertical, 4)
             
             // Path indicator
             LocalPathIndicatorView()
-            .padding(.bottom, 4)
+                .padding(.bottom, 4)
             
             // Local file list
             LocalPaneFileListView(
@@ -37,7 +39,8 @@ struct LocalPane: View {
         .onTapGesture {
             activePaneIndex = 1
         }
-        .dropDestination(for: String.self) { items, location in
+        .dropDestination(for: String.self) { items, _ in
+            handleFileDrop(items)
             return true
         }
     }
@@ -62,6 +65,75 @@ extension LocalPane {
         )
         showPreviewSheet = true
     }
+    
+    private func handleFileDrop(_ items: [String]) {
+        guard !items.isEmpty else { return }
+        
+        for item in items {
+            // Try to parse as JSON first
+            if let data = item.data(using: .utf8),
+               let fileInfo = try? JSONSerialization.jsonObject(with: data) as? [String: String],
+               let fileName = fileInfo["name"],
+               let isDirectoryStr = fileInfo["isDirectory"],
+               fileInfo["type"] == "smbFile" {
+                
+                // Get the file from SMB view model
+                guard let file = smbViewModel.getFileByName(fileName) else {
+                    return
+                }
+                
+                let isDirectory = isDirectoryStr == "true"
+                
+                Task {
+                    if isDirectory {
+                        // Handle folder download
+                        guard let destURL = viewModel.currentDirectory?.appendingPathComponent(fileName) else {
+                            return
+                        }
+                        
+                        await transferManager.startFolderDownload(
+                            folder: file,
+                            destination: destURL,
+                            smbViewModel: smbViewModel) {
+                                viewModel.refreshLocalFiles()
+                            }
+                        
+                    } else {
+                        // Handle single file download
+                        guard let destURL = viewModel.currentDirectory?.appendingPathComponent(fileName) else {
+                            return
+                        }
+                        
+                        await transferManager.startSingleFileDownload(
+                            file: file,
+                            destinationURL: destURL,
+                            smbViewModel: smbViewModel) {
+                                viewModel.refreshLocalFiles()
+                            }
+                    }
+                }
+                
+            } else {
+                // Try as simple file name
+                guard let file = smbViewModel.getFileByName(item) else {
+                    return
+                }
+                
+                Task {
+                    guard let destURL = viewModel.currentDirectory?.appendingPathComponent(file.name) else {
+                        return
+                    }
+                    
+                    await transferManager.startSingleFileDownload(
+                        file: file,
+                        destinationURL: destURL,
+                        smbViewModel: smbViewModel) {
+                            viewModel.refreshLocalFiles()
+                        }
+                }
+            }
+        }
+    }
 }
 
 struct LocalPane_Previews: PreviewProvider {
@@ -73,5 +145,6 @@ struct LocalPane_Previews: PreviewProvider {
         )
         .environmentObject(FileTransferViewModel())
         .environmentObject(LocalViewModel())
+        .environmentObject(TransferManager())
     }
 }
